4c151599456056263d1f6f9b0da2fd36fa3aae51
Matthew Heard <matthew.heard@modomoto.de>
2020-01-18 15:59:25 +0100

    Add first question

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,4 +2,8 @@ class Quiz
   def complete?
     false
   end
+
+  def first_question
+    'Which of the following capital cities is closest to the North Pole?'
+  end
 end
--- spec/quiz/quiz_spec.rb
+++ spec/quiz/quiz_spec.rb
@@ -6,5 +6,11 @@ describe Quiz do
       quiz = Quiz.new
       expect(quiz).to_not be_complete
     end
+
+    it 'has a first question' do
+      quiz = Quiz.new
+      expected_question = 'Which of the following capital cities is closest to the North Pole?'
+      expect(quiz.first_question).to eq expected_question
+    end
   end
 end
