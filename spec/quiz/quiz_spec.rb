9bed7ef79317d1f0fbfeb175a9bbc3f65b7d3644
Matthew Heard <matthew.heard@modomoto.de>
2020-01-18 13:46:08 +0100

    Define Quiz#complete?

--- spec/quiz/quiz_spec.rb
+++ spec/quiz/quiz_spec.rb
@@ -1,2 +1,16 @@
-class Quiz; end
-describe Quiz
+class Quiz
+  def complete?
+    false
+  end
+end
+
+describe Quiz do
+  describe '#complete?' do
+    context 'when no questions have been answered' do
+      it 'is not complete' do
+        quiz = Quiz.new
+        expect(quiz).to_not be_complete
+      end
+    end
+  end
+end
