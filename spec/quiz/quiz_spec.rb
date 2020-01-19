42ad84b08a952d89649c716785e6fe1588931b4a
Matthew Heard <matthew.heard@modomoto.de>
2020-01-19 22:12:07 +0100

    Extract test subject

--- spec/quiz/quiz_spec.rb
+++ spec/quiz/quiz_spec.rb
@@ -1,48 +1,43 @@
 require_relative '../../app/models/quiz'
 
 describe Quiz do
+  subject(:quiz) { Quiz.new }
+
   context 'when no questions have been answered' do
     it 'is not complete' do
-      quiz = Quiz.new
       expect(quiz).to_not be_complete
     end
 
     it 'sets the next question to be the first question' do
-      quiz = Quiz.new
       expected_question =
         'Fill in the blank: One Hundred ____ of Solitude by Gabriel García Márquez'
       expect(quiz.next_question.to_s).to eq expected_question
     end
 
     it 'has five options for answering the first question' do
-      quiz = Quiz.new
       expected_options = ['Hours', 'Days', 'Months', 'Years', 'Centuries']
       expect(quiz.next_question.options).to eq expected_options
     end
 
     it 'has 0 answered questions' do
-      quiz = Quiz.new
       expect(quiz.answer_count).to eq 0
     end
   end
 
   context 'after answering the first question' do
     it 'is not complete' do
-      quiz = Quiz.new
       first_question = quiz.next_question
       quiz.answer(first_question, anything)
       expect(quiz).to_not be_complete
     end
 
     it 'has 1 answered question' do
-      quiz = Quiz.new
       first_question = quiz.next_question
       quiz.answer(first_question, anything)
       expect(quiz.answer_count).to eq 1
     end
 
     it 'sets the next question to be the second question' do
-      quiz = Quiz.new
       first_question = quiz.next_question
       quiz.answer(first_question, anything)
       expected_question = 'Which Brontë sister wrote Jane Eyre?'
@@ -50,7 +45,6 @@ describe Quiz do
     end
 
     it 'has four options for answering the second question' do
-      quiz = Quiz.new
       first_question = quiz.next_question
       quiz.answer(first_question, anything)
       expected_options = ['Elizabeth', 'Charlotte', 'Emily', 'Anne']
@@ -59,9 +53,7 @@ describe Quiz do
   end
 
   context 'after answering the second question' do
-
     it 'is not complete' do
-      quiz = Quiz.new
       first_question = quiz.next_question
       quiz.answer(first_question, anything)
       second_question = quiz.next_question
@@ -70,7 +62,6 @@ describe Quiz do
     end
 
     it 'has 2 answered questions' do
-      quiz = Quiz.new
       first_question = quiz.next_question
       quiz.answer(first_question, anything)
       second_question = quiz.next_question
@@ -79,7 +70,6 @@ describe Quiz do
     end
 
     it 'sets the next question to be the third question' do
-      quiz = Quiz.new
       first_question = quiz.next_question
       quiz.answer(first_question, anything)
       second_question = quiz.next_question
@@ -89,7 +79,6 @@ describe Quiz do
     end
 
     it 'has three options for answering the third question' do
-      quiz = Quiz.new
       first_question = quiz.next_question
       quiz.answer(first_question, anything)
       second_question = quiz.next_question
