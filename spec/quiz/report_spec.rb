a034cdfebe2d3eb647019d8d22b71964c1894bbf
Matthew Heard <matthew.heard@modomoto.de>
2020-01-20 09:35:03 +0100

    Return first distribution

--- spec/quiz/report_spec.rb
+++ spec/quiz/report_spec.rb
@@ -1,2 +1,18 @@
-class Report; end
-describe Report
+class Report
+  def first_distribution
+    { 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0 }
+  end
+end
+
+describe Report do
+  it 'shows a distribution of answers to the first question' do
+    report = Report.new
+    expect(report.first_distribution).to eq({
+      1 => 0,
+      2 => 0,
+      3 => 0,
+      4 => 0,
+      5 => 0
+    })
+  end
+end
