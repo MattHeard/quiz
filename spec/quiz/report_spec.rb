1ce46c6bde63fcfd345b5e278d39264bf02ac3e3
Matthew Heard <matthew.heard@modomoto.de>
2020-01-20 09:36:18 +0100

    Wrap test in a context

--- spec/quiz/report_spec.rb
+++ spec/quiz/report_spec.rb
@@ -5,14 +5,16 @@ class Report
 end
 
 describe Report do
-  it 'shows a distribution of answers to the first question' do
-    report = Report.new
-    expect(report.first_distribution).to eq({
-      1 => 0,
-      2 => 0,
-      3 => 0,
-      4 => 0,
-      5 => 0
-    })
+  context 'when there are no quiz responses' do
+    it 'shows a distribution of answers to the first question' do
+      report = Report.new
+      expect(report.first_distribution).to eq({
+        1 => 0,
+        2 => 0,
+        3 => 0,
+        4 => 0,
+        5 => 0
+      })
+    end
   end
 end
