bd1eded17bc54b4233b1bf8a841a9a1f4b6d7fa7
Matthew Heard <matthew.heard@modomoto.de>
2020-01-22 23:38:21 +0100

    Extract all_complete_quizzes getter

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,17 +9,7 @@ class ReviewsController < ApplicationController
     @correct = (choice == question.correct_answer)
     @correct_answer = question.correct_answer
 
-    all_quiz_records = QuizRecord.where.not(q4: nil).all
-    all_quizzes = all_quiz_records.map do |other_record|
-      other_quiz = Quiz.new
-      %i[q0 q1 q2 q3 q4].each do |answer_key|
-        choice_index = other_record[answer_key].to_i
-        question = other_quiz.next_question
-        choice = question.option(choice_index)
-        other_quiz.answer(choice)
-      end
-      other_quiz
-    end
+    all_quizzes = repository.all_complete_quizzes
     @report = Report.new(all_quizzes)
   end
 end
--- app/repositories/quiz_repository.rb
+++ app/repositories/quiz_repository.rb
@@ -15,4 +15,18 @@ class QuizRepository
 
     quiz
   end
+
+  def all_complete_quizzes
+    all_quiz_records = QuizRecord.where.not(q4: nil).all
+    all_quiz_records.map do |other_record|
+      other_quiz = Quiz.new
+      %i[q0 q1 q2 q3 q4].each do |answer_key|
+        choice_index = other_record[answer_key].to_i
+        question = other_quiz.next_question
+        choice = question.option(choice_index)
+        other_quiz.answer(choice)
+      end
+      other_quiz
+    end
+  end
 end
