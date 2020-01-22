0a8e948e5c8c7a748c343fc1470dfb4d07f83e98
Matthew Heard <matthew.heard@modomoto.de>
2020-01-22 19:18:00 +0100

    Improve report answer styling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,13 +2,13 @@ class ReviewsController < ApplicationController
   def show
     quiz_id = session[:quiz_id]
     record = QuizRecord.find(quiz_id) if quiz_id
-    quiz = Quiz.new
+    @quiz = Quiz.new
     %i[q0 q1 q2 q3 q4].each do |answer_key|
       if record[answer_key]
         choice_index = record[answer_key].to_i
-        question = quiz.next_question
+        question = @quiz.next_question
         choice = question.option(choice_index)
-        quiz.answer(choice)
+        @quiz.answer(choice)
         @correct = (choice == question.correct_answer)
         @correct_answer = question.correct_answer
       end
@@ -28,6 +28,6 @@ class ReviewsController < ApplicationController
     @report = Report.new(all_quizzes)
     puts @report.inspect
 
-    redirect_to '/quiz' unless quiz.complete?
+    redirect_to '/quiz' unless @quiz.complete?
   end
 end
