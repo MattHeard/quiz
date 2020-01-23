e395d6506a7320e8d96af273426bff66420ceba4
Matthew Heard <matthew.heard@modomoto.de>
2020-01-23 12:58:51 +0100

    Redirect if quiz not complete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,13 +3,16 @@ class ReviewsController < ApplicationController
     quiz_id = session[:quiz_id]
     repository = QuizRepository.new
     @quiz = repository.find_by_id(quiz_id)
-    redirect_to '/quiz' unless @quiz.complete?
-    question = @quiz.previous_question
-    choice = @quiz.choice(question)
-    @correct = (choice == question.correct_answer)
-    @correct_answer = question.correct_answer
+    if @quiz.complete?
+      question = @quiz.previous_question
+      choice = @quiz.choice(question)
+      @correct = (choice == question.correct_answer)
+      @correct_answer = question.correct_answer
 
-    all_quizzes = repository.all_complete_quizzes
-    @report = Report.new(all_quizzes)
+      all_quizzes = repository.all_complete_quizzes
+      @report = Report.new(all_quizzes)
+    else
+      redirect_to '/quiz'
+    end
   end
 end
